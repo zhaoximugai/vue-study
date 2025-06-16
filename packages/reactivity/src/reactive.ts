11d52ff7057324f26d916916a11bb0ed1a015890
hejiacheng <1820715171@qq.com>
2025-06-16 15:47:01 +0800

    feat:reactive手写好像完成了

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,3 +1,48 @@
-export function reactive(){
-    
+import { isObject } from "@vue/shared";
+
+//reactiveMap 用于存储已经创建的代理对象
+//用于记录代理的对象，可以复用
+const reactiveMap = new WeakMap();
+
+enum ReactiveFlags {
+    IS_REACTIVE = '__v_isReactive',
+}
+const mutableHandlers: ProxyHandler<any> = {
+    get(target, key, receiver) {
+        if (key === ReactiveFlags.IS_REACTIVE) {
+            return true
+        }
+
+    },
+    set(target, key, value, receiver) {
+        return true
+
+    }
+}
+
+//reactive / shallowReactive
+export function reactive(target) {
+    return createReactiveObject(target);
+
+}
+
+function createReactiveObject(target) {
+    //判断是否是对象
+    if (!isObject(target)) {
+        return target
+    }
+
+    if (target[ReactiveFlags.IS_REACTIVE]) {
+        return target
+    }
+
+    const existProxy = reactiveMap.get(target);
+    //如果已经存在代理对象，则直接返回
+    if (existProxy) {
+        return existProxy
+    }
+    let proxy = new Proxy(target, mutableHandlers);
+    //根据对象缓存代理后的结果
+    reactiveMap.set(target, proxy)
+    return proxy
 }
